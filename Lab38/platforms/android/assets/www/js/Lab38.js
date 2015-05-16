d8d5039951d0774aac61468ee39e6f613cb991eb
Maru27 <marupachon@hotmail.com>
2015-05-15 21:51:11 -0500

    Ejercicio con Winre

---
+++ Lab38/platforms/android/assets/www/js/Lab38.js
@@ -0,0 +1,12 @@
+function Suma(){
+    console.log('Suma -> Ok');
+    var a=3;
+    var b=4;
+    var suma=a+b;
+    // Obteniendo la orientacion del dispositivo
+    console.log('Suma = ' + suma);
+    
+}
+
+Suma();
+
