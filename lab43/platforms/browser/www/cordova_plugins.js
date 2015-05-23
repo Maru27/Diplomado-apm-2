462890274dcfbcf86da4bf4171dbbe3b6e2e8cab
Maru27 <marupachon@hotmail.com>
2015-05-22 21:55:10 -0500

    dialogs

---
+++ lab43/platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
+        "id": "cordova-plugin-dialogs.notification_browser",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-dialogs": "1.1.0",
+    "cordova-plugin-vibration": "1.1.0"
+}
+// BOTTOM OF METADATA
+});
